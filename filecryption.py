c62ca0515c4d7b4dc09134dccdaa667f233e0ed9
frggycc <charliecardenas.pro@gmail.com>
2025-03-05 23:05:14 -0800

    Purged old code layout; Changes marked in README

--- filecryption.py
+++ filecryption.py
@@ -1,63 +1,82 @@
-# os for random salt
+# os for various functions
 # hashlib to use pbkdf2_hmac
-import sys, os
-import hashlib, binascii
+import os, sys
+import hashlib
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from Crypto.Util.Padding import unpad
 
-def generate_key(password, key_size):
-    salt = os.urandom(16)
-    iterations = 10000
-    aes_key = hashlib.pbkdf2_hmac("sha256", password.encode(), salt, iterations, key_size)
-    return aes_key, salt
+def generate_salt():
+    return os.urandom(16)
+def generate_iv():
+    return os.urandom(16)
+
+def generate_key(password, salt):
+    # Hash algorithm, passphrase, salt, iterations, key len in bytes
+    key = hashlib.pbkdf2_hmac("sha256", password.encode(), salt, 10000, 32)
+    return key
+
+'''---------------------------------------------------------------------'''
+''' Used for testing successful encrypting and decrypting '''
+def encrypt_message(message, password):
+    # Generate necessary 
+    salt = generate_salt()
+    iv   = generate_iv()
+    key  = generate_key(password, salt)
+
+    # Start creating cipher and encrypting 
+    plaintext = message.encode()
+    enc_cipher = AES.new(key, AES.MODE_CBC, iv)
+    padded_plain = pad(plaintext, 16)
+    cipher_text  = enc_cipher.encrypt(padded_plain)
+    return salt, cipher_text
+
+# def decrypt_message(encrypted_message, password, salt):
+#     # Generate key and extract iv and ciphertext
+#     key = generate_key(password, salt)
+#     iv  = encrypted_message[:16]
+#     ciphertext = encrypted_message[16:]
+
+#     # Start decrypting data
+#     dec_cipher  = AES.new(key, AES.MODE_CBC, iv)
+#     padded_data = dec_cipher.decrypt(cipher_text)
+#     decrypted_message = unpad(padded_data, 16)
     
-# Get action, file location, password to use
-# i.e. encrypt example.txt SuperStrongPassword
-# ACTION = str(sys.argv[1])
-# FILE   = str(sys.argv[2])
-# PASS   = str(sys.argv[3])
-# LENGTH = str(sys.argv[4])
+#     return decrypted_message
 
-MESSAGE = str(sys.argv[1])
-PASS    = str(sys.argv[2])
+'''---------------------------------------------------------------------'''
 
-''' 
-CREATING AES KEY FROM PASSPHRASES
------------------------------------------------------------------------
-Passphrase can be of any length. For the purpose of starting out this
-project, all key lengths will be 16-bytes so regardless of the
-passphrase size, the key generated will 16-bytes.
+''' Start of script '''
+if __name__ == "__main__":
 
-Additionally, we will make use of salting to make sure each time a key
-is generated using the passphrase, each key will be different from
-one another. Only the salt and the encrypted plaintext/file will be
-stored.
+    # Make sure that script is used correctly
+    if len(sys.argv) != 2:
+        print("Usage: python3 filecryption.py <message>")
+        exit()
 
-Passphrase + Salt       --> AES Key
-E(Plaintext  + AES Key) --> Encrypted Text
-Storage = Salt + Encrypted Text
+    message = sys.argv[1]
 
-Passphrase + Stored Salt    --> AES Key
-D(Encrypted Text + AES Key) --> Output Text
-Either text is decrypted or still encrypted. 
+    ''' 
+    User choice menu; When begin encrypting files, will also include
+    decrypting.
+    '''
+    print("1. Encrypt message")
+    print("2. Exit")
+    menu_choice = input("Enter your choice: ")
 
-FUTURE CHANGES: Maybe allow to configure it so that we hash any
-length passphrase to a desirable length/AES cipher type
-i.e. Configure to always use AES-128, use passphrase to produce
-a 16 byte hash digest.
-'''
-# Get new aes_key to use and salt to save into file
-aes_key, salt = generate_key(PASS, 16)
+    # Get user password or exit program
+    if menu_choice != "2":
+        ''' *** Add password function from password library '''
+        password = input("Enter Password: ")
+    else:
+        print("Leaving filecryption...")
+        exit()
 
-# Store the salt and encrypted file name
-
-# Encrypt plaintext using that AES key
-plaintext = MESSAGE.encode()
-encCipher = AES.new(aes_key, AES.MODE_ECB)
-# Pad the text to be multiples of 16
-paddedPlain = pad(plaintext, 16)
-cipherText = encCipher.encrypt(paddedPlain)
-print("My Plaintext Message:", MESSAGE)
-print("Encrypted Plaintext :", end=" ")
-print(cipherText)
+    if menu_choice == "1":
+        print("Here is your message in plaintext : " + message)
+        salt, cipher_text = encrypt_message(message, password)
+        print("Here is your message encrypted   : ", end=" ")
+        print(cipher_text)
+        # decrypted_message = decrypt_message(cipher_text, password, salt)
+        # print("Here is your message after decryption: " + decrypted_message)
+        # exit()
